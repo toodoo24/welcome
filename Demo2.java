06273c03a08a79c7e97a1a72d91254f46b3c2821
TooDoo <146521393+toodoo24@users.noreply.github.com>
2025-03-14 17:10:04 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,19 @@
+package com.welcome;
+
+public class Demo2 {
+	
+	static int empid=24;
+	static String empname= "Vignesh";
+	static double empsal = 30000.00;
+	static String role = "Develpoer";
+	public static void main(String[] args) {
+		Demo2 obj = new Demo2();
+		int mark = 45;
+		String name ="Vignesh";
+		System.out.println("Static varible is :"+ empid);
+		System.out.println("local variavle is :"+ name);
+		
+		
+	}
+
+}
